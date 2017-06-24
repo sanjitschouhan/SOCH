98f98f964c45ecc04bd6b41a2694a61f9f1275e5
sanjit <sanjitschouhan@gmail.com>
2017-06-25 05:09:33 +0530

    reports Page

---
+++ reports/submit.php
@@ -0,0 +1,17 @@
+<?php
+$desc = $_COOKIE['description'];
+$problem = $_COOKIE['problem'];
+$city = $_COOKIE['city'];
+$state = $_COOKIE['state_id'];
+$conn = mysqli_connect("localhost", "root", "harsha", "soch");
+$result = mysqli_query($conn, "select id from cities where name='" . $city . "' and state_id=" . $state);
+$id = mysqli_fetch_assoc($result)['id'];
+setcookie("city_id", $id);
+$rows = array();
+mysqli_query($conn, "insert into grevience('problem','description','city_id') VALUES($problem,$desc, $id)");
+mysqli_close($conn);
+?>
+
+<script>
+    window.open("/speak/","_self");
+</script>
--- speak/problems.php
+++ speak/problems.php
@@ -1,20 +1,20 @@
 <?php
-$problem = $_COOKIE['problem'];
-$city = $_COOKIE['city'];
-$state = $_COOKIE['state_id'];
-$conn = mysqli_connect("localhost", "root", "harsha", "soch");
-$result = mysqli_query($conn, "select id from cities where name='" . $city . "' and state_id=" . $state);
-$id = mysqli_fetch_assoc($result)['id'];
-setcookie("city_id", $id);
-$rows = array();
-$result = mysqli_query($conn, "select * from problems where city_id=" . $id . " and problem='" . $problem . "'");
-while ($r = mysqli_fetch_assoc($result)) {
-    $rows[] = $r;
-}
-echo "<script>";
-echo "var probs = " . json_encode($rows);
-echo "</script>";
-?>
+//$problem = $_COOKIE['problem'];
+//$city = $_COOKIE['city'];
+//$state = $_COOKIE['state_id'];
+//$conn = mysqli_connect("localhost", "root", "harsha", "soch");
+//$result = mysqli_query($conn, "select id from cities where name='" . $city . "' and state_id=" . $state);
+//$id = mysqli_fetch_assoc($result)['id'];
+//setcookie("city_id", $id);
+//$rows = array();
+//$result = mysqli_query($conn, "select * from problems where city_id=" . $id . " and problem='" . $problem . "'");
+//while ($r = mysqli_fetch_assoc($result)) {
+//    $rows[] = $r;
+//}
+//echo "<script>";
+//echo "var probs = " . json_encode($rows);
+//echo "</script>";
+//?>
 
 <html lang="en">
 <head>
@@ -27,15 +27,16 @@ echo "</script>";
     <title>SOCH</title>
 </head>
 <body>
-<table class="table table-hover" style="width: 70%; margin: 0 auto;">
+<table class="table table-bordered" style="width: 70%; text-align: center; margin: 0 auto;">
     <thead>
     <tr>
-        <th>Description</th>
-        <th>Up Votes</th>
-        <th>Down Votes</th>
+        <th style="text-align: center">Description</th>
+        <th style="text-align: center">Up Votes</th>
+        <th style="text-align: center">Down Votes</th>
     </tr>
     </thead>
     <tbody id="problem-list">
+    <tr><td>Drainage System </td><td><div class="btn" onclick="like(&quot;2&quot;)"><i id="like-2" class="fa fa-thumbs-o-up"></i><div style="display: inline;" id="upvotes-2">5</div></div></td><td><div class="btn" onclick="dislike(&quot;2&quot;)"><i id="dislike-2" class="fa fa-thumbs-o-down"></i><div style="display: inline;" id="downvotes-2">2</div></div></td></tr><tr><td>Drainage dfjs </td><td><div class="btn" onclick="like(&quot;3&quot;)"><i id="like-3" class="fa fa-thumbs-o-up"></i><div style="display: inline;" id="upvotes-3">5</div></div></td><td><div class="btn" onclick="dislike(&quot;3&quot;)"><i id="dislike-3" class="fa fa-thumbs-o-down"></i><div style="display: inline;" id="downvotes-3">2</div></div></td></tr><tr><td>Drainage dfjssfsd </td><td><div class="btn" onclick="like(&quot;4&quot;)"><i id="like-4" class="fa fa-thumbs-o-up"></i><div style="display: inline;" id="upvotes-4">5</div></div></td><td><div class="btn" onclick="dislike(&quot;4&quot;)"><i id="dislike-4" class="fa fa-thumbs-o-down"></i><div style="display: inline;" id="downvotes-4">2</div></div></td></tr>
     <script>
         function like(id) {
             $("#like-" + id).removeClass("fa-thumbs-o-up").addClass("fa-thumbs-up");
