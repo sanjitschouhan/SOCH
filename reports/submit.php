ffcece64de2e319fbf64fe756db2c4e457e9a8cf
sanjit <sanjitschouhan@gmail.com>
2017-06-25 11:47:30 +0530

    reports Page

--- reports/submit.php
+++ reports/submit.php
@@ -1,5 +1,5 @@
 <?php
-$desc = $_COOKIE['description'];
+$desc = $_COOKIE['desc'];
 $problem = $_COOKIE['problem'];
 $city = $_COOKIE['city'];
 $state = $_COOKIE['state_id'];
@@ -7,8 +7,10 @@ $conn = mysqli_connect("localhost", "root", "harsha", "soch");
 $result = mysqli_query($conn, "select id from cities where name='" . $city . "' and state_id=" . $state);
 $id = mysqli_fetch_assoc($result)['id'];
 setcookie("city_id", $id);
+mysqli_close($conn);
+$conn = mysqli_connect("localhost", "root", "harsha", "soch");
 try {
-    $sql = "INSERT INTO `problems` (`id`, `problem`, `description`, `city_id`, `upvotes`, `downvotes`) VALUES (NULL, \'$problem\', \'$desc\', \'$id\', \'0\', \'0\')";
+    $sql = "INSERT INTO `problems` (`id`, `problem`, `description`, `city_id`, `upvotes`, `downvotes`) VALUES (NULL, '$problem', '$desc', '$id', '0', '0')";
     mysqli_query($conn, $sql);
 } catch (Exception $e) {
     echo $e->getMessage();
@@ -17,5 +19,5 @@ mysqli_close($conn);
 ?>
 
 <script>
-    window.open("/speak/problems.php", "_self");
+    window.open("/", "_self");
 </script>
