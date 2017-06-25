196a5629aa1c93825a5da4b08e0775e1b73243b9
sanjit <sanjitschouhan@gmail.com>
2017-06-25 13:11:43 +0530

    reports Page

--- jquery-3.1.1.js
+++ jquery-3.1.1.js
@@ -1271,10 +1271,10 @@ setDocument = Sizzle.setDocument = function( node ) {
 
 	// QSA and matchesSelector support
 
-	// matchesSelector(:active) reports false when true (IE9/Opera 11.5)
+	// matchesSelector(:active) grievance false when true (IE9/Opera 11.5)
 	rbuggyMatches = [];
 
-	// qSa(:focus) reports false when true (Chrome 21)
+	// qSa(:focus) grievance false when true (Chrome 21)
 	// We allow this because of a bug in IE8/9 that throws an error
 	// whenever `document.activeElement` is accessed on an iframe
 	// So, we allow :focus to pass through QSA all the time to avoid the IE error
